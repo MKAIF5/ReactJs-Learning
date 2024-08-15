bf8b4e576df06960958c1e7c1e83a969a38d2afa
mohammed kaif <mkaif555123@gmail.com>
2024-08-16 00:47:00 +0500

    try for-in loop

--- Custom Reactjs/script.js
+++ Custom Reactjs/script.js
@@ -1,15 +1,26 @@
 const customRender = (elementReact, container) => {
+    // const domElement = document.createElement(elementReact.type);
+    // domElement.innerHTML = elementReact.children;
+    // domElement.setAttribute("href", elementReact.props.href);
+    // domElement.setAttribute("target", elementReact.props.target);
+    // container.appendChild(domElement);
+
+    //try for-in loop
+
     const domElement = document.createElement(elementReact.type);
     domElement.innerHTML = elementReact.children;
-    domElement.setAttribute("href", elementReact.props.href);
-    domElement.setAttribute("target", elementReact.props.target);
+
+    for (const prop in elementReact.props) {
+        domElement.setAttribute(prop, elementReact.props[prop])
+    }
+
     container.appendChild(domElement);
 }
 
 const elementReact = {
     type: "a",
     props: {
-        href: "https//google.com",
+        href: "https://google.com",
         target: "_blank"
     },
     children: "clicked the button"
