ce830f334cefe788598f9a8b522955025a36c2e1
mohammed kaif <mkaif555123@gmail.com>
2024-08-21 19:49:34 +0500

    counter added

--- Hooks ReactJs/hooks practice/src/App.jsx
+++ Hooks ReactJs/hooks practice/src/App.jsx
@@ -2,13 +2,18 @@ import { useState } from "react";
 import "./App.css";
 
 let counter = 5;
+const addValue = () =>{
+  counter = counter + 1
+  console.log("value add" , counter);
+  
+}
 
 function App() {
   return (
     <>
       <h1>Create Counter</h1>
       <p> Counter Value : {counter}</p>
-      <button>Add Value</button>
+      <button onClick={addValue}>Add Value</button>
       <button>Remove Value</button>
     </>
   );
