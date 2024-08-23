3663b8b808fcbb9df75e9240d00dff6160e6a8d7
mohammed kaif <mkaif555123@gmail.com>
2024-08-23 20:58:59 +0500

    props and tailwand practice continue

--- Tailwand Props ReactJs/tailwand-props-practice/src/App.jsx
+++ Tailwand Props ReactJs/tailwand-props-practice/src/App.jsx
@@ -5,9 +5,6 @@ import "./App.css";
 import Kaif from "./kaif.jsx";
 
 function App() {
-  // const [count, setCount] = useState(0)
-
-  // console.log(props);
   let myObj = {
     name: "kaif",
     age: 15,
@@ -18,6 +15,8 @@ function App() {
   return (
     <>
       <h1 className="bg-green-600 text-white p-4 rounded-lg">Tailwand Css </h1>
+      <Kaif arr={myArr} />
+      <Kaif arr={myArr} />
       <Kaif arr={myArr} />
     </>
   );
