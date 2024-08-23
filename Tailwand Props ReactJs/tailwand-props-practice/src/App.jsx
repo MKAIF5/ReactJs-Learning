cd9502a57554349c22bab75d72d64716656bbf0a
mohammed kaif <mkaif555123@gmail.com>
2024-08-23 20:03:48 +0500

    props learning start

--- Tailwand Props ReactJs/tailwand-props-practice/src/App.jsx
+++ Tailwand Props ReactJs/tailwand-props-practice/src/App.jsx
@@ -1,16 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Kaif from "./kaif.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
+  // console.log(props);
+  let myObj = {
+    name: "kaif",
+    age: 15,
+    isMarried: false,
+  };
+
+  let myArr = [1, 2, 3, 4, 5];
   return (
     <>
-      <h1 className='bg-green-600 text-white p-4 rounded-lg hover:m-1' >Tailwand Css </h1>
+      <h1 className="bg-green-600 text-white p-4 rounded-lg">Tailwand Css </h1>
+      <Kaif arr={myArr} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ Tailwand Props ReactJs/tailwand-props-practice/src/kaif.jsx
@@ -0,0 +1,11 @@
+function Kaif(props) {
+    console.log(props);
+    
+    return (
+        <>
+        <h1>Kaif Hu Mai</h1>
+        </>
+    )
+}
+
+export default Kaif
