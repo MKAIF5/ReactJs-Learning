d8436348418e6c961401d70231bc9d5b2c33042e
mohammed kaif <mkaif555123@gmail.com>
2024-09-18 19:47:07 +0500

    second practice on counter

--- Hooks ReactJs/hooks practice/src/App.jsx
+++ Hooks ReactJs/hooks practice/src/App.jsx
@@ -5,25 +5,25 @@ function App() {
 
   let [counter, setCounter] = useState(15);
 
-  const addValue = () => {
-    counter = counter + 1
-    setCounter(counter)
-  }
+  // const addValue = () => {
+  //   counter = counter + 1
+  //   setCounter(counter)
+  // }
 
-  const subValue = () =>{
-    counter = counter - 1
-    setCounter(counter)
-  }
+  // const subValue = () =>{
+  //   counter = counter - 1
+  //   setCounter(counter)
+  // }
 
-  if(counter === 0){
-    counter = 15
-  }
+  // if(counter === 0){
+  //   counter = 15
+  // }
 
   return (
     <>
       <h1>Counter {counter}</h1>
-      <button onClick={addValue}>Add Value</button>
-      <button onClick={subValue}>Decrease Value</button>
+      <button onClick={() => setCounter(counter + 1)}>Add Value</button>
+      <button onClick={() => setCounter(counter - 1)}>Decrease Value</button>
 
     </>
   )
