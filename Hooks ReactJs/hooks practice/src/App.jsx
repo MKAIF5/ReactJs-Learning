2e874d01f73fa7831484fc20a69c23b32a0cbdc8
mohammed kaif <mkaif555123@gmail.com>
2024-10-06 14:47:35 +0500

    calculator practice

--- Hooks ReactJs/hooks practice/src/App.jsx
+++ Hooks ReactJs/hooks practice/src/App.jsx
@@ -3,7 +3,6 @@ import "./App.css";
 
 function App() {
 
-  let [counter, setCounter] = useState(15);
 
   // const addValue = () => {
   //   counter = counter + 1
@@ -21,9 +20,65 @@ function App() {
 
   return (
     <>
-      <h1>Counter {counter}</h1>
-      <button onClick={() => setCounter(counter + 1)}>Add Value</button>
-      <button onClick={() => setCounter(counter - 1)}>Decrease Value</button>
+      <div className='bg-white w-96 p-4 mx-auto my-8 rounded-xl'>
+        <h1 className='text-center text-2xl font-bold'>CALCULATOR</h1>
+        <div className='flex justify-center my-5'>
+          <input
+            type="text"
+            className='bg-gray-800 rounded-lg h-24 w-80 text-white p-1 text-right text-3xl font-semibold outline-none'
+            readOnly
+          />
+        </div>
+        <br />
+        <div className='flex justify-center gap-1 mb-2'>
+          <button className='p-4 bg-gray-400 border border-black text-black
+           rounded-lg w-20 text-xl font-semibold'>AC</button>
+          <button className='p-4 bg-gray-400 border border-black text-black
+           rounded-lg w-20 text-xl font-semibold'>DE</button>
+          <button className='p-4 bg-gray-400 border border-black text-black
+           rounded-lg w-20 text-xl font-semibold'>.</button>
+          <button className='p-4 bg-gray-400 border border-black text-black
+           rounded-lg w-20 text-xl font-semibold'>/</button>
+        </div>
+        <div className='flex justify-center gap-1 mb-2'>
+          <button className='p-4 bg-gray-400 border border-black text-black
+           rounded-lg w-20 text-xl font-semibold' >7</button>
+          <button className='p-4 bg-gray-400 border border-black text-black
+           rounded-lg w-20 text-xl font-semibold' >8</button>
+          <button className='p-4 bg-gray-400 border border-black text-black
+           rounded-lg w-20 text-xl font-semibold' >9</button>
+          <button className='p-4 bg-gray-400 border border-black text-black
+           rounded-lg w-20 text-xl font-semibold' >*</button>
+        </div>
+        <div className='flex justify-center gap-1 mb-2'>
+          <button className='p-4 bg-gray-400 border border-black text-black 
+          rounded-lg w-20 text-xl font-semibold' >4</button>
+          <button className='p-4 bg-gray-400 border border-black text-black
+           rounded-lg w-20 text-xl font-semibold' >5</button>
+          <button className='p-4 bg-gray-400 border border-black text-black
+           rounded-lg w-20 text-xl font-semibold' >6</button>
+          <button className='p-4 bg-gray-400 border border-black text-black
+           rounded-lg w-20 text-xl font-semibold' >+</button>
+        </div>
+        <div className='flex justify-center gap-1 mb-2'>
+          <button className='p-4 bg-gray-400 border border-black text-black
+           rounded-lg w-20 text-xl font-semibold' >1</button>
+          <button className='p-4 bg-gray-400 border border-black text-black
+           rounded-lg w-20 text-xl font-semibold' >2</button>
+          <button className='p-4 bg-gray-400 border border-black text-black
+           rounded-lg w-20 text-xl font-semibold' >3</button>
+          <button className='p-4 bg-gray-400 border border-black text-black
+           rounded-lg w-20 text-xl font-semibold' >-</button>
+        </div>
+        <div className='flex justify-center gap-1 mb-2'>
+          <button className='p-4 bg-gray-400 border border-black text-black
+           rounded-lg w-20 text-xl font-semibold' >00</button>
+          <button className='p-4 bg-gray-400 border border-black text-black
+           rounded-lg w-20 text-xl font-semibold' >0</button>
+          <button className='p-4 bg-gray-800 border border-black text-white
+           rounded-lg w-40 text-xl font-semibold'>=</button>
+        </div>
+      </div>
 
     </>
   )
