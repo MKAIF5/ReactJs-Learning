9a864cc8bf76c7d6e7f5c4b9b666250c0f4ad313
mohammed kaif <mkaif555123@gmail.com>
2024-10-08 19:55:07 +0500

    password generator functionality and UI 50% Doen

--- Hooks ReactJs/hooks practice/src/App.jsx
+++ Hooks ReactJs/hooks practice/src/App.jsx
@@ -1,13 +1,77 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import "./App.css";
 
 function App() {
 
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState("");
+
+
+  const passwordGenerator = useCallback(() => {
+
+    let pass = ""
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    if (numberAllowed) {
+      str += "0123456789";
+    }
+
+    if (charAllowed) {
+      str += "~!@#$%^&*()_-{}[]<>,|"
+    }
+
+
+  }, [length, numberAllowed, charAllowed, setPassword])
 
   return (
     <>
 
-
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg mt-8 px-3 py-3 text-orange-500 bg-slate-700">
+        <h1 className="text-white text-center">Password Generator</h1>
+        <div className="flex shadow rounded-lg overflow-hidden mb-4 my-3">
+          <input
+            className="outline-none w-full py-1 px-3"
+            type="text"
+            placeholder="password"
+            readOnly
+          />
+          <button
+            className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0"
+            onClick={() => {
+              navigator.clipboard.writeText(password);
+            }}
+          >
+            Copy
+          </button>
+        </div>
+        <div className="flex text-sm gap-x-2">
+          <div className="flex items-center gap-x-1">
+            <input
+              className="cursor-pointer"
+              type="range"
+              min={6}
+              max={50}
+            />
+            <label>Length: ({length})</label>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              id="numberInput"
+            />
+            <label>Numbers</label>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              id="characterInput"
+            />
+            <label>Characters</label>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
