751a31781fb1c6bf8ba44f505e923ae9f3d4dbaa
mohammed kaif <mkaif555123@gmail.com>
2024-10-24 17:20:09 +0500

    files cleaning up

--- Tailwand Props ReactJs/tailwand-props-practice/src/App.jsx
+++ Tailwand Props ReactJs/tailwand-props-practice/src/App.jsx
@@ -1,22 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import Kaif from "./kaif.jsx";
 
 function App() {
-   let myObj = {
-   name: "kaif",
-  age: 15,
-    isMarried: false,
-  };
-
-  // let myArr = [1, 2, 3, 4, 5];
   return (
     <>
-      <h1 className="bg-green-600 text-white p-4 rounded-lg">Tailwand Css </h1>
-      <Kaif username="Kaif hu mai" />
-      <Kaif username="mai 15 saal ka hu" />
+
+
     </>
   );
 }
