92334311cc2432ae29ebfe93cab0bfe9d7657500
mohammed kaif <mkaif555123@gmail.com>
2024-10-24 19:21:32 +0500

    make components

--- Props ReactJs/Props/src/App.jsx
+++ Props ReactJs/Props/src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 function App() {
   return (
     <>
-      <h1 className='text-center text-green-500 text-3xl'>Kaif</h1>
+    
     </>
   )
 }
---
+++ Props ReactJs/Props/src/Components/Kaif.jsx
@@ -0,0 +1,11 @@
+function Kaif() {
+    return (
+        <>
+        <div>
+            <h1></h1>
+        </div>
+        </>
+    )
+}
+
+export default Kaif
