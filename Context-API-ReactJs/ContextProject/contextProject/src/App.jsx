1d54a82a1f763ce2ab806b9119e5a9b07f736c79
mohammed kaif <mkaif555123@gmail.com>
2025-01-03 10:56:17 +0500

    Theme Project Completed

--- Context-API-ReactJs/ContextProject/contextProject/src/App.jsx
+++ Context-API-ReactJs/ContextProject/contextProject/src/App.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react'
 import { ThemeProvider } from './context/theme'
 import { useEffect } from 'react';
+import ThemeBtn from './components/ThemeButton';
+import Card from './components/Card';
+import "./App.css"
 
 const App = () => {
 
@@ -15,8 +18,8 @@ const App = () => {
   };
 
   useEffect(() => {
-      document.querySelector("html").classList.remove("light" , "dark");
-      document.querySelector("html").classList.add(themeMode);
+    document.querySelector("html").classList.remove("light", "dark");
+    document.querySelector("html").classList.add(themeMode);
   }, [themeMode]);
 
   return (
@@ -24,11 +27,11 @@ const App = () => {
       <div className="flex flex-wrap min-h-screen items-center">
         <div className="w-full">
           <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-            {/* ThemeButton UI */}
+            <ThemeBtn />
           </div>
 
           <div className="w-full max-w-sm mx-auto">
-            {/* Card UI */}
+            <Card />
           </div>
         </div>
       </div>
