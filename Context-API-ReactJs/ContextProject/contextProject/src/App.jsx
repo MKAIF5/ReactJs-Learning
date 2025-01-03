59b544c8b7a2f50c3c6245f9060169195ed6c6dc
mohammed kaif <mkaif555123@gmail.com>
2025-01-03 10:35:30 +0500

    theme 50% Work Completed

--- Context-API-ReactJs/ContextProject/contextProject/src/App.jsx
+++ Context-API-ReactJs/ContextProject/contextProject/src/App.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { ThemeProvider } from './context/theme'
 
 const App = () => {
+
+  const [themeMode, setThemeMode] = useState("light");
+
+  const lightTheme = () => {
+    setThemeMode("light");
+  };
+
+  const darkTheme = () => {
+    setThemeMode("dark");
+  };
+
   return (
-    <>
-    
-    </>
+    <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            {/* ThemeButton UI */}
+          </div>
+
+          <div className="w-full max-w-sm mx-auto">
+            {/* Card UI */}
+          </div>
+        </div>
+      </div>
+
+    </ThemeProvider>
   )
 }
 
