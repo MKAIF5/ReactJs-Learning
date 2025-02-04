e4daae2cfab8be376409543f67c14aa6a11bba2a
mohammed kaif <mkaif555123@gmail.com>
2025-02-04 14:53:22 +0500

    functionality 70% Work Completed

--- useEffect, useRef and useCallback ReactJs/useEffect useRef and useCallback/src/App.jsx
+++ useEffect, useRef and useCallback ReactJs/useEffect useRef and useCallback/src/App.jsx
@@ -1,5 +1,4 @@
 import { useCallback, useState } from 'react';
-import './App.css'
 
 function App() {
 
@@ -31,6 +30,54 @@ function App() {
   return (
     <>
 
+      <div className='w-full max-w-md mx-auto shadow-md px-4 py-3 my-8 rounded-lg
+      text-orange-500 bg-gray-700'>
+        <h1 className='text-white text-center my-3'>Password Generator</h1>
+        <div className='flex shadow rounded-lg overflow-hidden mb-4'>
+          <input
+            type="text"
+            value={password}
+            className='outline-none w-full py-1 px-3'
+            placeholder='Password'
+            readOnly
+          />
+          <button className='outline-none bg-blue-700 text-white
+          px-3 py-0.5 shrink-0'>Copy</button>
+        </div>
+        <div className='flex text-sm gap-x-2'>
+          <div className='flex items-center gap-x-1'>
+            <input
+              type="range"
+              min={8}
+              max={50}
+              value={length}
+              className='cursor-pointer'
+              onChange={(e) => { setLength(e.target.value) }}
+            />
+            <label>length: {length}</label>
+          </div>
+          <div className='flex items-center gap-x-1'>
+            <input
+              type="checkbox"
+              defaultChecked={numberAllowed}
+              onChange={() => {
+                setNumberAllowed((prev) => !prev);
+              }}
+            />
+            <label>Numbers</label>
+          </div>
+          <div className='flex items-center gap-x-1'>
+            <input
+              type="checkbox"
+              defaultChecked={numberAllowed}
+              onChange={() => {
+                setCharAllowed((prev) => !prev);
+              }}
+            />
+            <label>Characters</label>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
